878e3e67f85c0ed2cb360735e1a460bc74caa6ec
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-12 21:07:10 +0530

    Added habit model

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Habits(models.Model):
+    name = models.CharField()
+    author = models.CharField()
+
+
+# class User(models.Model):
+#
