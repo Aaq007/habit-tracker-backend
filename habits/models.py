e14597e0e2493352eca4cc56d43b53dc367fe566
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-14 22:35:18 +0530

    Modified habit model

--- habits/models.py
+++ habits/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
 
 
 class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=32)
-    # author = models.CharField(max)
