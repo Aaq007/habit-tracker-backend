bfd9c99976a4f575ec44a10c415b056cda53d485
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-15 12:40:32 +0530

    Added update url

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HabitListView
+from .views import HabitListView, HabitUpdateView
 
 
 urlpatterns = [
     path('', HabitListView.as_view()),
+    path('<int:pk>/', HabitUpdateView.as_view()),
 ]
