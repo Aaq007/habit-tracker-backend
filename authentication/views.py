43a23da428177697aed9f2600e6918912fd71c65
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-18 23:04:36 +0530

    Added UserLogiinView

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import User
+from .serializers import UserLoginSerializer
 # Create your views here.
+
+
+class UserLoginView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserLoginSerializer
