02347a98d6cd321e667048d483f2a183940b3576
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-20 19:59:10 +0530

    Added authentication urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import UserCreateView, UserLoginView
 urlpatterns = [
-    # path('login/',),
+    path('login/', UserLoginView.as_view()),
+    path('create/', UserCreateView.as_view()),
 ]
