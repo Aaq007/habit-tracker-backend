71b4c5be472ccbdb332a7150b5162ac56371da32
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-21 14:50:55 +0530

    changed auth/create/ to auth/register/

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,5 @@ from .views import UserCreateView, UserLoginView
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('login/', obtain_auth_token),
-    path('create/', UserCreateView.as_view()),
+    path('register/', UserCreateView.as_view()),
 ]
