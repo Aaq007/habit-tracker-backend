317ab4611fe67a26ffde72a42b36dc0b8271fbc2
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-21 14:50:16 +0530

    Added obtaib_auth_token view to url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UserCreateView, UserLoginView
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
-    path('login/', UserLoginView.as_view()),
+    path('login/', obtain_auth_token),
     path('create/', UserCreateView.as_view()),
 ]
