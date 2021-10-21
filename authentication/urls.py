30e8a96b30fa89ea93a8fb53c68bb01b136ae335
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-21 19:09:15 +0530

    added userlistview to urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserCreateView, UserLoginView
+from .views import UserCreateView, UserListView, UserLoginView
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('login/', obtain_auth_token),
     path('register/', UserCreateView.as_view()),
+    path('users/', UserListView.as_view()),
+
 ]
